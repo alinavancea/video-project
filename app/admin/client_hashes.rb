14e10f26236d357ff9a5d678df2bd64e1d004035
Popa Adrian Marius <mariuz@php.net>
2012-03-30 18:32:11 +0300

    created the model links between hashes<->comments

--- app/admin/client_hashes.rb
+++ app/admin/client_hashes.rb
@@ -1,3 +1,8 @@
 ActiveAdmin.register ClientHash do
+    index do
+        column "Hashtag", :text
+        column "Created At", :created_at
+        default_actions
+    end
   
 end
--- app/models/client_hash.rb
+++ app/models/client_hash.rb
@@ -2,6 +2,9 @@ class ClientHash < ActiveRecord::Base
   belongs_to :client
   has_many :comments
   has_many :embed_videos
+  has_many :product_promotions
+  has_many :comments , :through =>:hashes_comments
+  
   def display_name
         text
   end
