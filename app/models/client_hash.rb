9c983a23ed06e2ff0ad61de0750f46fb16a8688a
Popa Adrian Marius <mariuz@php.net>
2012-03-30 17:30:31 +0300

    fixed display name for client,hashes

--- app/models/client_hash.rb
+++ app/models/client_hash.rb
@@ -2,12 +2,9 @@ class ClientHash < ActiveRecord::Base
   belongs_to :client
   has_many :comments
   has_many :embed_videos
-<<<<<<< HEAD
   def display_name
         text
   end
-=======
 
   validates :text, :presence => true
->>>>>>> c590a7fdecb8d3153337ad4b15cf36c8e0736a18
 end
