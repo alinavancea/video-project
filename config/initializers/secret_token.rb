d685c4b06d50f5e82eaa7cfcd7bbc8a5facf5495
alina <alinavancea@reea.net>
2012-03-30 11:32:35 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoProject::Application.config.secret_token = 'c5f49c01c6b765705ecc6b3dc69febb13cee14278dce5f4e58b01c11e8bbe83fa2912c4bffa33427dd372081b38285ae5deab62cf1223d430bd64df3d92e24f4'
