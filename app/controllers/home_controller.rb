c2c2eeb78ca4125d0b8d2ebba82625a7126987cc
alina <alinavancea@reea.net>
2012-03-30 13:28:48 +0300

    updates in views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
-    
+    hash_id = params[:hash_id]
+    @videos = []
+    begin
+
+      hash = ClientHash.find(hash_id)
+      @unlocked_video = hash.embed_videos.first
+      count = hash.counter
+      @videos = hash.embed_videos
+    rescue ActiveRecord::RecordNotFound
+      
+    end
   end
 end
--- app/models/embed_video.rb
+++ app/models/embed_video.rb
@@ -1,3 +1,21 @@
 class EmbedVideo < ActiveRecord::Base
   belongs_to :client_hash
+
+  #after_create :lock
+  
+  def small_video
+    
+  end
+
+  def is_locked?
+    true
+  end
+
+  def unlock
+    
+  end
+  
+  def lock
+
+  end
 end
