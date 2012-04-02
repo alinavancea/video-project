81ccf75c044e0063164e8f39564ae6b70fce181e
Popa Adrian Marius <mapopa@reea.net>
2012-04-02 18:25:06 +0300

    added the Comments panel and Videos sidebar on the right

--- app/admin/client_hashes.rb
+++ app/admin/client_hashes.rb
@@ -4,5 +4,41 @@ ActiveAdmin.register ClientHash do
         column "Created At", :created_at
         default_actions
     end
+    
+    show :title => proc { resource.text } do
+        section :class => "panel" do
+            h3 "Hash Details"
+            div do
+                attributes_table_for resource do
+                    row("Hash Text")     { resource.text }
+                    row("Created At")    { resource.created_at }
+                    
+                end
+            end
+        end
+            
+
+        section  :class => "panel panel-table" do
+            h3 "Comments"
+            ul :class => "promotion-ul" do
+                resource.comments.each do |c|
+                    li link_to( c.text, admin_hash_comment_path(c) )
+                end
+            end
+        end
+        
+                
+    end
+    
+    sidebar "Videos", :only => [:show] do
+          h3 "Videos"
+            ul :class => "promotion-ul" do
+                resource.embed_videos.each do |v|
+                    li link_to( v.text, admin_embed_video_path(v) )
+                end
+            end
+        
+        end
+
   
 end
