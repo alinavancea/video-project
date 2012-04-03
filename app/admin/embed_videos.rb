3f13ba72aa56ab1da3050051b7e0dc6462fb543a
Popa Adrian Marius <mapopa@reea.net>
2012-04-03 17:12:26 +0300

    added position ajax controller

--- app/admin/embed_videos.rb
+++ app/admin/embed_videos.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register EmbedVideo do
+  config.clear_sidebar_sections!
+
+    index do
+        div  render :partial => "admin/embed_videos/index",:class => "main_content_wrapper"
+    end
+
+    controller do
+        def save_position(object={}, &block)
+            hash = params[:hash]
+            hash.size().times do |i|
+                video = EmbedVideo.find(hash[i])
+                video.position = i
+                video.save
+            end
+            render :nothing => true
+        end
+    end
+
   
 end
