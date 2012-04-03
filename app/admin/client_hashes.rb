ba0e5f9f716a26eba25618ef491a256c7e3a3351
Popa Adrian Marius <mapopa@reea.net>
2012-04-03 14:54:34 +0300

    added custom style

--- app/admin/client_hashes.rb
+++ app/admin/client_hashes.rb
@@ -20,7 +20,7 @@ ActiveAdmin.register ClientHash do
 
         section  :class => "panel panel-table" do
             h3 "Comments"
-            ul :class => "promotion-ul" do
+            ul :class => "nomarker-ul" do
                 resource.comments.each do |c|
                     li link_to( c.text, admin_hash_comment_path(c) )
                 end
@@ -32,7 +32,7 @@ ActiveAdmin.register ClientHash do
     
     sidebar "Videos", :only => [:show] do
           h3 "Videos"
-            ul :class => "promotion-ul" do
+            ul :class => "nomarker-ul" do
                 resource.embed_videos.each do |v|
                     li link_to( v.text, admin_embed_video_path(v) )
                 end
